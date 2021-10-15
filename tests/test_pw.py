e27d1deb50aa4eaec28459eb82d75f3dff08692d
ihoubr <ivo.houbrechts@nike.com>
2021-10-15 15:38:26 +0200

    version 2.0.0 with support for generic aliases s Please enter the commit message for your changes. Lines starting

--- tests/test_pw.py
+++ tests/test_pw.py
@@ -18,156 +18,108 @@ def test_toml_wraptor_value():
     assert pw.toml_wraptor_value(toml, "tool4").split() == ["d", "e"]
     assert pw.toml_wraptor_value(toml, "tool5") == "tool5 pip"
     assert pw.toml_wraptor_value(toml, "tool6") == "tool6 pip"
+    assert pw.toml_wraptor_value(toml, "xxx") is None
 
 
 def test_toml_aliases():
     toml = Path(__file__).with_name("test.toml")
     assert pw.toml_aliases(toml) == {
         "run": "tool1 start",
-        "test": "tool2 run test",
+        "test": "tool-2 run test",
         "sub_tool": "tool1 : sub_tool default_arg",
         "sub-tool-2-alias": "tool-2 : sub-tool-2 default:arg",
-        "combined-alias": "& : run test",
-        "all-aliases": "& : combined-alias sub_tool sub-tool-2-alias shell-command",
-        "shell-command": "!: ls -al",
+        "combined-alias": "./pw run && ./pw test ./pw shell-command",
+        "shell-command": "ls -al",
     }
 
 
-def test_parse_args():
-    calls, upgrade, clear = pw.parse_args(["pw", "command"])
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "command"
-    assert args == []
+def test_parse_args_with_tool():
+    toml = Path(__file__).with_name("test.toml")
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "tool1"], toml)
+    assert tool == "tool1"
+    assert cmd == "tool1"
     assert not upgrade
     assert not clear
 
-    calls, upgrade, clear = pw.parse_args(["pw", "command", "arg1", "arg2"])
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "command"
-    assert args == ["arg1", "arg2"]
-    assert not upgrade
-    assert not clear
-
-    calls, upgrade, clear = pw.parse_args(["pw", "--upgrade", "command", "arg1"])
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "command"
-    assert args == ["arg1"]
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "tool1", "--pw-upgrade", "arg1", "arg2"], toml)
+    assert tool == "tool1"
+    assert cmd == "tool1 arg1 arg2"
     assert upgrade
     assert not clear
 
-    calls, upgrade, clear = pw.parse_args(["pw", "--clear", "command"])
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "command"
-    assert args == []
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "tool-2"], toml)
+    assert tool == "tool-2"
+    assert cmd == "tool-2"
+    assert not upgrade
+    assert not clear
+
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "tool3", "arg"], toml)
+    assert tool == "tool3"
+    assert cmd == "tool3 arg"
+    assert not upgrade
+    assert not clear
+
+
+def test_parse_args_no_tool():
+    toml = Path(__file__).with_name("test.toml")
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "command"], toml)
+    assert tool is None
+    assert cmd == "command"
+    assert not upgrade
+    assert not clear
+
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "command", "arg1", "arg2"], toml)
+    assert tool is None
+    assert cmd == "command arg1 arg2"
+    assert not upgrade
+    assert not clear
+
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "--pw-upgrade", "command", "arg1"], toml)
+    assert tool is None
+    assert cmd == "command arg1"
+    assert upgrade
+    assert not clear
+
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "--pw-clear", "command"], toml)
+    assert tool is None
+    assert cmd == "command"
     assert not upgrade
     assert clear
 
-    with pytest.raises(Warning, match=f"Python Wraptor version {pw.VERSION}"):
-        pw.parse_args(["pw", "--version", "command"])
-
     with pytest.raises(Warning, match=re.escape(pw.HELP)):
-        pw.parse_args(["pw"])
+        pw.parse_args(["path/to/pw"], toml)
 
 
 def test_parse_args_with_alias():
     toml = Path(__file__).with_name("test.toml")
     aliases = pw.toml_aliases(toml)
 
-    calls, upgrade, clear = pw.parse_args(["pw", "run"], aliases)
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "tool1"
-    assert args == ["start"]
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "run"], toml, aliases)
+    assert tool == "tool1"
+    assert cmd == "tool1 start"
     assert not upgrade
     assert not clear
 
-    calls, upgrade, clear = pw.parse_args(["pw", "--clear", "test", "my-test"], aliases)
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "tool2"
-    assert args == ["run", "test", "my-test"]
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "test", "my-test", "--pw-clear"], toml, aliases)
+    assert tool == "tool-2"
+    assert cmd == "tool-2 run test my-test"
     assert not upgrade
     assert clear
 
-    calls, upgrade, clear = pw.parse_args(["pw", "--upgrade", "sub_tool", "arg1", "arg2"], aliases)
-    tool, cmd, args = calls[0]
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "sub_tool", "--pw-upgrade", "arg1", "arg2"], toml, aliases)
     assert tool == "tool1"
-    assert cmd == "sub_tool"
-    assert args == ["default_arg", "arg1", "arg2"]
+    assert cmd == "sub_tool default_arg arg1 arg2"
     assert upgrade
     assert not clear
 
-    calls, upgrade, clear = pw.parse_args(["pw", "sub-tool-2-alias", "arg1", "arg2"], aliases)
-    tool, cmd, args = calls[0]
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "sub-tool-2-alias", "arg1", "arg2"], toml, aliases)
     assert tool == "tool-2"
-    assert cmd == "sub-tool-2"
-    assert args == ["default:arg", "arg1", "arg2"]
+    assert cmd == "sub-tool-2 default:arg arg1 arg2"
     assert not upgrade
     assert not clear
 
-
-def test_parse_args_with_combined_alias():
-    toml = Path(__file__).with_name("test.toml")
-    aliases = pw.toml_aliases(toml)
-
-    calls, upgrade, clear = pw.parse_args(["pw", "combined-alias"], aliases)
-    assert len(calls) == 2
-
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "tool1"
-    assert args == ["start"]
-    assert not upgrade
-    assert not clear
-
-    tool, cmd, args = calls[1]
-    assert tool == cmd == "tool2"
-    assert args == ["run", "test"]
-    assert not upgrade
-    assert not clear
-
-    calls, upgrade, clear = pw.parse_args(["pw", "all-aliases"], aliases)
-    assert len(calls) == 5
-
-    tool, cmd, args = calls[0]
-    assert tool == cmd == "tool1"
-    assert args == ["start"]
-    assert not upgrade
-    assert not clear
-
-    tool, cmd, args = calls[1]
-    assert tool == cmd == "tool2"
-    assert args == ["run", "test"]
-    assert not upgrade
-    assert not clear
-
-    tool, cmd, args = calls[2]
-    assert tool == "tool1"
-    assert cmd == "sub_tool"
-    assert args == ["default_arg"]
-    assert not upgrade
-    assert not clear
-
-    tool, cmd, args = calls[3]
-    assert tool == "tool-2"
-    assert cmd == "sub-tool-2"
-    assert args == ["default:arg"]
-    assert not upgrade
-    assert not clear
-
-    tool, cmd, args = calls[4]
-    assert tool == "!"
-    assert cmd == "ls"
-    assert args == ["-al"]
-    assert not upgrade
-    assert not clear
-
-
-def test_parse_args_with_shell_command():
-    toml = Path(__file__).with_name("test.toml")
-    aliases = pw.toml_aliases(toml)
-
-    calls, upgrade, clear = pw.parse_args(["pw", "shell-command"], aliases)
-    tool, cmd, args = calls[0]
-    assert tool == "!"
-    assert cmd == "ls"
-    assert args == ["-al"]
+    tool, cmd, upgrade, clear = pw.parse_args(["path/to/pw", "combined-alias", "arg1", "arg2"], toml, aliases)
+    assert tool is None
+    assert cmd == "path/to/pw run && path/to/pw test path/to/pw shell-command arg1 arg2"
     assert not upgrade
     assert not clear
